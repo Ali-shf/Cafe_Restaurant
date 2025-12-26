0f9cbfe5ef84bb22e5a80c25737bbe8b059c500e
alishf <ali88206497@gmail.com>
2025-12-26 22:38:38 +0330

    Add Uploading multiple files feature

--- menu/models.py
+++ menu/models.py
@@ -1,15 +1,28 @@
 from django.db import models
 from authentication.models import *
+from cloudinary.models import CloudinaryField
+
 
 class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    image = models.ImageField(upload_to="menu_images/")
-    dish_id=models.IntegerField(default=0)
+    dish_id = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
+
+
+class MenuItemImage(models.Model):
+    menu_item = models.ForeignKey(
+        MenuItem,
+        related_name='images',
+        on_delete=models.CASCADE,
+    )
+    image = CloudinaryField('image')
+
+    def __str__(self) -> str:
+        return f'Image for {self.menu_item.name}'
     
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
